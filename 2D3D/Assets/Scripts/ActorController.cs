d973b419c35a7e70e7c8fc73f3d97646e8eeed63
Kit <kit@cerebralfix.com>
2014-11-03 18:15:04 +1300

    inaugural commit

---
+++ 2D3D/Assets/Scripts/ActorController.cs
@@ -0,0 +1,203 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent (typeof(CharacterAnimator))]
+public class ActorController : MonoBehaviour 
+{
+	public enum EControledBy
+	{
+		Invalid = -1,
+		Human,
+		Computer,
+		Max
+	}
+	public EControledBy ControlledBy;
+
+	public enum ECharacterStatus
+	{
+		Normal = 0,
+		Slowed,
+		Sitting,
+		Stunned,
+		Frozen,
+		Dead,
+	}
+	public ECharacterStatus Status;
+
+	public enum EDirection
+	{
+		Invalid = -1,
+		North,
+		Northeast,
+		East,
+		Southeast,
+		South,
+		Southwest,
+		West,
+		Northwest,
+		Directionless // for animations with only one or no apparent direction
+	}
+	EDirection direction = EDirection.South;
+	public EDirection Direction
+	{
+		get 
+		{
+			return direction;
+		}
+		set 
+		{
+			lastDirection = direction;
+			direction = value;
+		}
+	}
+	
+	bool CanMove
+	{
+		get
+		{
+			if (Status == ECharacterStatus.Normal)	return true;
+			else return false;
+		}
+	}
+	
+	bool CanChangeDirection
+	{
+		get
+		{
+			if (Status == ECharacterStatus.Normal)	return true;
+			else return false;
+		}
+	}
+
+	const float SPEEDSCALE = 300;
+	const float TIMETOTOPSPEED = 0.5f;
+	const float AXIS_DEADZONE = 0.001f;
+	public CharacterAnimator characterAnimator;
+	EDirection lastDirection = EDirection.Invalid;
+	float timeFacingThisDirection =0f;
+	Vector2 directionValue;
+	Vector2 inputValue;
+	bool requireChange = false;
+	float MovementSpeed = 1.5f;
+	InputDevice input;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		switch (ControlledBy)
+		{
+		case EControledBy.Human:
+		{
+			if(Input.GetKeyDown(KeyCode.Space)) {
+				characterAnimator.callJump();
+			}
+			if (InputMovement())
+			{
+				// begin accelerating character
+				Vector2 movement = Vector3.zero;
+				movement.x = Input.GetAxis("Horizontal");
+				movement.y = Input.GetAxis("Vertical");
+
+				Move (movement);
+
+//				if (rigidbody.velocity.magnitude < 1.5f)
+//				{
+//					rigidbody.AddForce(
+//						movement.normalized * SPEEDSCALE * MovementSpeed * Time.smoothDeltaTime, 
+//						ForceMode.Force
+//						);
+//				}
+
+			}
+			else
+			{
+				// no movement this frame
+				characterAnimator.UpdateMovementSpeed(0f);
+			}
+		}
+		break;
+		case EControledBy.Computer:
+		{
+
+		}
+		break;
+
+		}
+	}
+		
+	void Move (Vector2 direction)
+	{
+		if (CanChangeDirection)
+		{
+			transform.forward = new Vector3(direction.x, 0, direction.y);
+				
+			// send input to animator
+			Vector2 cartesian;
+			
+			// GetAxis smooths keyboard input, resulting in a delay when inputs go from 1 to 0
+			//  this doesn't matter much in gameplay, but looks bad in the animator.
+			//  for this reason, we use GetAxisRaw for that instead.
+			
+			//  TODO : This assumes keyboard input, test and address potential gamepad issues.
+			cartesian.x = Input.GetAxisRaw("Horizontal");
+			cartesian.y = Input.GetAxisRaw("Vertical");
+			characterAnimator.UpdateDirection(cartesian);
+		}
+
+		if (CanMove)
+		{
+			if (rigidbody.velocity.magnitude < 1.5f)
+			{
+				rigidbody.AddForce(
+					transform.forward * SPEEDSCALE * MovementSpeed * Time.smoothDeltaTime, 
+					ForceMode.Force
+					);
+				characterAnimator.UpdateMovementSpeed(1f);
+			}
+		}
+	}
+
+	public static bool InputMovement()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		horizontal = Mathf.Abs(horizontal) > AXIS_DEADZONE ? horizontal : 0f;
+		vertical = Mathf.Abs(vertical) > AXIS_DEADZONE ? vertical : 0f;
+		return !((horizontal == 0) && (vertical == 0));
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.layer == LayerMask.NameToLayer("Default")) return;
+
+		if (other.gameObject.layer != gameObject.layer)
+		{
+			//if (other.transform.name == "feet")
+			if (ControlledBy == EControledBy.Computer)
+			{
+				// push!
+				Vector3 direction = -1 * (other.transform.position-transform.position);
+
+				rigidbody.AddForce(direction * 500);
+
+				Debug.Log(direction);
+			}
+//			else
+//			{
+//
+//			}
+		}
+		//Debug.Log("Collided with " + other.ToString());
+	}
+
+	void OnDrawGizmos()
+	{
+//		Gizmos.color = Color.yellow;
+//		Gizmos.DrawRay(transform.position, transform.forward*100f);
+	}
+}
---
+++ 2D3D/Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,232 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAnimator : MonoBehaviour {
+
+	public enum EDirection
+	{
+		Invalid = -1,
+		North,
+		Northeast,
+		East,
+		Southeast,
+		South,
+		Southwest,
+		West,
+		Northwest,
+		Directionless // for animations with only one or no apparent direction
+	}
+
+	public enum EAnimType
+	{
+		Invalid = -1,
+		Idle,
+		Sitting,
+		Sleeping,
+		Walk,
+		Hover,
+		Fly,
+		Kick,
+		Punch,
+		Stomp,
+		TakingDamage,
+		Faint,
+		Special
+	}
+
+	Animator animator;
+	EAnimType currentState = EAnimType.Idle;
+	public EAnimType CurrentState
+	{
+		get 
+		{
+			return currentState;
+		}
+		set 
+		{
+			if (currentState != value)
+			{
+				currentState = value;
+				requireChange = true;
+				SetAnimType();
+			}
+		}
+	}
+	EDirection direction = EDirection.South;
+	public EDirection Direction
+	{
+		get 
+		{
+			return direction;
+		}
+		set 
+		{
+			lastDirection = direction;
+			direction = value;
+		}
+	}
+
+	EDirection lastDirection = EDirection.Invalid;
+	float timeFacingThisDirection =0f;
+	const float TIMETOMOVE = 0.3f;
+	Vector2 directionValue;
+	bool requireChange = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+		GetComponent<SpriteRenderer>().castShadows = true;
+		GetComponent<SpriteRenderer>().receiveShadows = true;
+	}
+	
+//	// Update is called once per frame
+//	void Update () 
+//	{
+////		if (requireChange)
+////		{			
+////			animator.SetTrigger("ChangeState");
+////			requireChange = false;
+////		}
+//
+//		if (PlayerController.InputMovement())
+//		{
+//
+//			EDirection newDirection = ParseInputDirection();
+//
+//			if (newDirection == Direction)
+//			{
+//				timeFacingThisDirection += Time.smoothDeltaTime;
+//				if (CurrentState == EAnimType.Idle)
+//				{
+//					if (timeFacingThisDirection > TIMETOMOVE)
+//					{
+//						// set animtype to walking
+//						CurrentState = EAnimType.Walk;
+//						SetSpeed(1f);
+//					}
+//				}
+//			}
+//			else
+//			{
+//				Direction = newDirection;
+//				timeFacingThisDirection = 0f;
+//			}
+//
+//			SetDirection();
+//
+//		}
+//	}
+
+	public void callJump() {
+		animator.SetTrigger("Jump");
+	}
+
+	public void UpdateDirection(Vector2 _input)
+	{
+		EDirection newDirection = ParseInputDirection(_input);
+
+		SetDirection();
+	}
+
+	public void UpdateMovementSpeed(float _speed)
+	{
+		SetSpeed(_speed);
+	}
+
+	EDirection ParseInputDirection(Vector2 _inputDirection)
+	{
+		Vector2 inputDirection = _inputDirection;
+		
+		if (inputDirection == Vector2.zero)
+		{
+			return EDirection.Invalid;
+		}
+
+		if (inputDirection.x > 0) // face right
+		{
+			if (inputDirection.y > 0)
+			{
+				directionValue.y = 1;
+				directionValue.x = 1;
+				return EDirection.Northeast;
+			}
+			else if (inputDirection.y < 0)
+			{
+				directionValue.y = -1;
+				directionValue.x = 1;
+				return EDirection.Southeast;
+			}
+			else
+			{
+				directionValue.y = 0;
+				directionValue.x = 1;
+				return EDirection.East;
+			}
+		}
+		else if (inputDirection.x < 0) // face left
+		{
+			if (inputDirection.y > 0)
+			{
+				directionValue.y = 1;
+				directionValue.x = -1;
+				return EDirection.Northwest;
+			}
+			else if (inputDirection.y < 0)
+			{
+				directionValue.y = -1;
+				directionValue.x = -1;
+				return EDirection.Southwest;
+			}
+			else
+			{
+				directionValue.y = 0;
+				directionValue.x = -1;
+				return EDirection.West;
+			}			
+		}
+		else
+		{
+			if (inputDirection.y > 0)
+			{
+				directionValue.y = 1;
+				directionValue.x = 0;
+				return EDirection.North;
+			}
+			else
+			{
+				directionValue.y = -1;
+				directionValue.x = 0;
+				return EDirection.South;
+			}
+		}
+	}
+
+	void SetAnimType()
+	{
+		Debug.Log(currentState);
+		animator.SetInteger("AnimType", (int)currentState);
+	}
+
+	void Sit()
+	{
+		animator.SetTrigger("Sit");
+	}
+
+	void SetSpeed(float _speed)
+	{
+		animator.SetFloat("Speed", _speed);
+	}
+
+	void SetDirection()
+	{
+		animator.SetFloat("MoveX", directionValue.x);
+		animator.SetFloat("MoveY", directionValue.y);
+	}
+//	
+//	void OnDrawGizmos()
+//	{
+//		Gizmos.color = Color.yellow;
+//		Gizmos.DrawLine(transform.position, Camera.main.transform.position);
+//	}
+}
