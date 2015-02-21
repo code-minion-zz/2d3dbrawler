bcf08fa1e6a5022422a01dc80ea8f9b08a18fdf4
Kit <chanwk3@gmail.com>
2015-02-22 12:27:54 +1300

    pushing some new stuff

---
+++ 2D3D/Assets/CatAnimator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatAnimator : MonoBehaviour {
+
+    private SpriteRenderer renderer;
+    private SpriteManager.SpriteAnimation spriteSheet;
+    private int currentFrame = 0;
+    private int maxFrames;
+    private float frameAccum = 0;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<SpriteRenderer>();
+        renderer.color = Color.grey;
+        spriteSheet = SpriteManager.Instance.GetAnimation("Idle");
+        maxFrames = spriteSheet.sprites.Count;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        frameAccum += Time.deltaTime;
+
+        if (frameAccum > spriteSheet.frameDelay)
+        {
+            frameAccum = 0;
+            ++currentFrame;
+            if (currentFrame >= maxFrames)
+            {
+                currentFrame = 0;
+            }
+            renderer.sprite = spriteSheet.sprites[currentFrame];
+        }
+	}
+}
---
+++ 2D3D/Assets/SpriteManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+using Assets.Scripts;
+
+public class SpriteManager : MonoBehaviour {
+
+    public static SpriteManager Instance;
+
+    [Serializable]
+    public class SpriteAnimation
+    {
+        public string name;
+        public float frameDelay;
+        public List<Sprite> sprites;
+    }
+
+    public List<SpriteAnimation> animations;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public SpriteAnimation GetAnimation(string name)
+    {
+        return animations.Where(item=> item.name == name).First();
+    }
+}
